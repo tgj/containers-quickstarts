92826ce93fb4c92649763e329114ad9cd37dc725
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2016-10-24 20:52:54 -0700

    f

--- s2i-datapower/usr/bin/fixpermissions.sh
+++ s2i-datapower/usr/bin/fixpermissions.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Fix permissions on the given directory to allow group read/write of
 # regular files and execute of directories.
-chown -R gogs "$1"
-chgrp -R 0 "$1"
-chmod -R g+rw "$1"
-find "$1" -type d -exec chmod g+x {} +
+/bin/busybox chown -R gogs "$1"
+/bin/busybox chgrp -R 0 "$1"
+/bin/busybox chmod -R g+rw "$1"
+/bin/busybox find "$1" -type d -exec /bin/busybox chmod g+x {} +
