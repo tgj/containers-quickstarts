5bb0e1287c0fc94140aee8b8fab670d344dfb6fd
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2016-12-07 10:04:47 -0500

    f

--- s2i-datapower/usr/bin/fixpermissions.sh
+++ s2i-datapower/usr/bin/fixpermissions.sh
@@ -2,6 +2,8 @@
 # Fix permissions on the given directory to allow group read/write of
 # regular files and execute of directories.
 /bin/busybox chown -LR default "$1"
+/bin/busybox chown -h default "$1"
 /bin/busybox chgrp -LR 0 "$1"
+/bin/busybox chgrp -h 0 "$1"
 /bin/busybox chmod -R g+rw "$1"
 /bin/busybox find "$1" -type d -exec /bin/busybox chmod g+x {} +
